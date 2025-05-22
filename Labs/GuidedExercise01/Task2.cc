5f5b67c6fd40e29b0dea2ceeea7600797faa0ff3
alooboii <27100125@lums.edu.pk>
2025-05-22 10:39:09 +0500

    GE 1 done

--- Labs/GuidedExercise01/Task2.cc
+++ Labs/GuidedExercise01/Task2.cc
@@ -7,6 +7,19 @@ int main()
 
     // Prompt the user to enter the height of the triangle
     // write code here
+    printf("Enter the height of the triangle: ");
+    scanf("%d", &height);
+
+    file = fopen("triangle.txt", "w");
+
+    for (int i = 0; i < height; i++) {
+        for (int j = 0; j <= i; j++) {
+            fprintf(file, "%c", '*');
+        }
+        fprintf(file, "\n");
+    }
+
+    fclose(file);
 
     printf("Right angle triangle has been written to triangle.txt\n");
 
